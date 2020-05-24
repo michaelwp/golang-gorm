159f89379956c4ea9bd78763f942831b70e747a1
Michael WP <michaelwp@Michaels-MacBook-Air.local>
2020-05-24 14:25:53 +0700

    create find email

--- controllers/controller.go
+++ controllers/controller.go
@@ -23,17 +23,22 @@ func AddUser(w http.ResponseWriter, r *http.Request) {
 	res.Message = "Data successfully inserted"
 	w.Header().Set("Content-type", "application/json")
 
-	mySql := db.MySql()
-
 	err := json.NewDecoder(r.Body).Decode(&u)
 	if err != nil {log.Fatal(err)}
+
+	err, _ = findEmail(u.Email)
+
+	if err == nil {
+		errhandlers.ErrCreate(w, res, "Email already registered")
+		return
+	}
 
 	user := models.User{
 		FirstName: strings.ToLower(u.FirstName),
 		LastName: strings.ToLower(u.LastName),
 	}
 
-	userRes := mySql.Create(&user)
+	userRes := db.MySql().Create(&user)
 	if userRes.Error != nil {
 		errhandlers.ErrCreate(w, res, fmt.Sprintf("%s", userRes.Error))
 		return
@@ -44,15 +49,25 @@ func AddUser(w http.ResponseWriter, r *http.Request) {
 			Password: u.Password,
 		}
 
-		credRes := mySql.Create(&cred)
+		credRes := db.MySql().Create(&cred)
 		if credRes.Error != nil {
 			errhandlers.ErrCreate(w, res, fmt.Sprintf("%s", credRes.Error))
 			return
 		}
 	}
 
-	mySql.Close()
+	defer db.MySql().Close()
 
 	err = json.NewEncoder(w).Encode(res)
 	if err != nil {log.Fatal(err)}
+}
+
+func findEmail(email string) (error, models.Credential) {
+	var c models.Credential
+
+	credRes := db.MySql().
+		Where("email like ?", "%" + email + "%").
+		Find(&c)
+
+	return credRes.Error, c
 }
