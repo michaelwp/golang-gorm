8b4be45916494834a41dbaefca8a501876d4d5b2
Michael WP <michaelwp@Michaels-MacBook-Air.local>
2020-05-26 18:27:40 +0700

    create middleware for authenticating token

--- routers/router.go
+++ routers/router.go
@@ -3,14 +3,18 @@ package routers
 import (
 	"github.com/gorilla/mux"
 	"github.com/michaelwp/golang-gorm/controllers"
+	"github.com/michaelwp/golang-gorm/middlewares"
 )
 
 func Router() *mux.Router{
 	router := mux.NewRouter()
 
-	router.HandleFunc("/", controllers.Home).Methods("GET")
-	router.HandleFunc("/user", controllers.AddUser).Methods("POST")
 	router.HandleFunc("/login", controllers.Login).Methods("POST")
+	router.HandleFunc("/", controllers.Home).Methods("GET")
+
+	auth := router.PathPrefix("/auth").Subrouter()
+	auth.Use(middlewares.IsAuthorized)
+	auth.HandleFunc("/user", controllers.AddUser).Methods("POST")
 
 	return router
 }
